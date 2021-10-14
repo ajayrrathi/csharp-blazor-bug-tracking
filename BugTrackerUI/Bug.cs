0ac903dbbc2b6e533317d6e018b7da13119c1876
ajayrrathi <ajay.r.rathi@gmail.com>
2021-10-14 19:46:10 -0400

    Completed assigment.

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BugTrackerUI.Pages;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,11 +10,13 @@ namespace BugTrackerUI
     public class Bug
     {
         public int Id { get; set; }
-
+        [Required(ErrorMessage ="Please enter Title.")]
         public string Title { get; set; }
-
+        [Required(ErrorMessage ="Please enter Description.")]
+        [MinLength(10)]
         public string Description { get; set; }
-
+        [Required(ErrorMessage ="Please enter Priority.")]
+        [Range(1,5)]
         public int Priority { get; set; }
     }
 }
